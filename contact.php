2c3df3557df06e34f3d7dd6d72a126419a6d7e42
unknown <assiatourabi55@gmail.com>
2024-04-27 02:39:45 +0100

    contact modification

--- contact.php
+++ contact.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($_POST['submit'])) {
-    $to = 'votre@email.com'; // Remplacez par votre adresse email
+    $to = 'assiatourabi55@email.com'; 
     $subject = $_POST['subject'];
     $message = $_POST['message'];
     $headers = "From: " . $_POST['full_name'] . " <" . $_POST['email'] . ">\r\n";
     $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
     
     if (mail($to, $subject, $message, $headers)) {
-        echo '<script>alert("Votre message a bien été envoyé. Nous vous contacterons bientôt.");</script>';
+        echo '<script>alert("Votre message a bien été envoyé.");</script>';
     } else {
         echo '<script>alert("Une erreur s\'est produite lors de l\'envoi du message. Veuillez réessayer plus tard.");</script>';
     }
